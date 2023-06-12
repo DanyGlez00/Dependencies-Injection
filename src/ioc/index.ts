36b5e1352ac6ed28d233c5b8fb81705c93ffc07c
DanyGlez00 <dgonzalez@griddynamics.com>
2023-06-11 23:07:36 -0600

    🏷️ [ADD]: Generic type added to IoCContainer

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -4,8 +4,8 @@ import { Logger } from '../services/logger';
 import { HTTP } from '../services/http';
 import { Users } from '../services/users';
 
-export const createIoCContainer = () => {
-  const ioc = new IoCContainer();
+export const createIoCContainer = (): IoCContainer<any> => {
+  const ioc = new IoCContainer<any>();
   
   ioc.registerClass('logger', Logger);
   ioc.registerClass('http', HTTP);
