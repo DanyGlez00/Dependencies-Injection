df409ed70164b0aef20ba168eb1589ea5c25ef72
DanyGlez00 <dgonzalez@griddynamics.com>
2023-06-12 01:50:04 -0600

    🏷️ [UPDATE]: Any type removed, created a generic interface instead

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -3,10 +3,18 @@ import IoCContainer from 'ioc-lite';
 import { Logger } from '../services/logger';
 import { HTTP } from '../services/http';
 import { Users } from '../services/users';
+import type { IApiConfig } from '../types';
 
-export const createIoCContainer = (): IoCContainer<any> => {
-  const ioc = new IoCContainer<any>();
-  
+interface ContainerResources {
+  logger: typeof Logger;
+  http: typeof HTTP;
+  users: typeof Users;
+  apiConfig: IApiConfig;
+}
+
+export const createIoCContainer = (): IoCContainer<ContainerResources> => {
+  const ioc = new IoCContainer<ContainerResources>();
+
   ioc.registerClass('logger', Logger);
   ioc.registerClass('http', HTTP);
   ioc.registerClass('users', Users);
